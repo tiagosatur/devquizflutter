bfac0658a8c71b1098c4bf582590dd95c26e7993
tiagosatur <tiagosatur@gmail.com>
2021-04-20 18:11:03 -0300

    Add level button widget

--- lib/home/widgets/appbar/appbar_widget.dart
+++ lib/home/widgets/appbar/appbar_widget.dart
@@ -38,7 +38,7 @@ class AppBarWidget extends PreferredSize {
               ),
             ),
             Align(
-              alignment: Alignment(0.0, 1.2),
+              alignment: Alignment(0.0, 1.0),
               child: ScoreCardWidget())
           ],
         )
