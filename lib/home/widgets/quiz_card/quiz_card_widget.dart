08749e823b975845c76ca9f5008ffba38ad65636
tiagosatur <tiagosatur@gmail.com>
2021-04-21 18:54:32 -0300

    Create challenge page, progress indicator and question indicator widgets

--- lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -1,6 +1,7 @@
 import 'package:devquizflutter/core/app_colors.dart';
 import 'package:devquizflutter/core/app_images.dart';
 import 'package:devquizflutter/core/app_text_styles.dart';
+import 'package:devquizflutter/shared/widgets/progress_indicator/progress_indicator_widget.dart';
 import 'package:flutter/material.dart';
 
 class QuizCardWidget extends StatelessWidget {
@@ -34,11 +35,7 @@ class QuizCardWidget extends StatelessWidget {
               ),
               Expanded(
                 flex: 3,
-                child: LinearProgressIndicator(
-                  value: 0.3,
-                  backgroundColor: AppColors.chartSecondary,
-                  valueColor:  AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
-                ),
+                child: ProgressIndicatorWidget(value: 0.3),
               )
             ],
           )
